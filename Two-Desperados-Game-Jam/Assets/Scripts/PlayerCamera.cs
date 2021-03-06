636cf45e1051af01208432b0eb51ac143c51385f
Milos Stojicic <as.stojicic@gmail.com>
2021-03-06 16:19:06 +0100

    Player movement script

---
+++ Two-Desperados-Game-Jam/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour{
+
+    public Transform m_player;
+    public Transform m_camera;
+    public float m_xSensitivity;
+    public float m_ySensitivity;
+    public float m_maxAngle;
+    private Quaternion m_camCenter;
+    public static bool cursorLocked = true;
+    void Start(){
+        m_camCenter = m_camera.localRotation;
+    }
+
+    void Update(){
+        setY();
+        setX();
+        updateCursorLock();
+    }
+
+    void setY(){
+        float input = Input.GetAxis("Mouse Y") * m_ySensitivity * Time.deltaTime;
+        Quaternion adj = Quaternion.AngleAxis(input, -Vector3.right);
+        Quaternion delta = m_camera.localRotation * adj;
+        if(Quaternion.Angle(m_camCenter, delta) < m_maxAngle){
+            m_camera.localRotation = delta;
+        }
+    }
+    void setX(){
+        float input = Input.GetAxis("Mouse X") * m_xSensitivity * Time.deltaTime;
+        Quaternion adj = Quaternion.AngleAxis(input, Vector3.up);
+        Quaternion delta = m_player.localRotation * adj;
+        m_player.localRotation = delta;
+    }
+    void updateCursorLock(){
+        if (cursorLocked){
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (Input.GetKeyDown(KeyCode.Escape)) cursorLocked = false;
+        }
+        else{
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            if (Input.GetKeyDown(KeyCode.Escape)) cursorLocked = true;
+        }
+    }
+}
---
+++ Two-Desperados-Game-Jam/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotion : MonoBehaviour{
+
+    public CharacterController m_controller;
+    public float m_speed;
+    public float m_sprintSpeed;
+    public float m_gravity;
+
+    Vector3 m_velocity;
+    private float m_verticalAxis;
+    private float m_horizontalAxis;
+
+    
+    void Update(){
+        // Axes
+        m_verticalAxis = Input.GetAxis("Vertical");
+        m_horizontalAxis = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * m_horizontalAxis + transform.forward * m_verticalAxis;
+        if(Input.GetKey(KeyCode.LeftShift) && m_verticalAxis > 0) m_controller.Move(move * m_sprintSpeed * Time.deltaTime);
+        else m_controller.Move(move * m_speed * Time.deltaTime);
+
+        m_velocity.y += m_gravity * Time.deltaTime;
+        m_controller.Move(m_velocity * Time.deltaTime);
+    }
+}
